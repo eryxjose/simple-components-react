307381bdddb378c72c038198b371d4c8ad888fb7
Eryx <eryxjose@hotmail.com>
2023-02-23 02:05:41 -0300

    Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import Link from './components/Link';
+import Sidebar from './components/Sidebar';
 import Route from './components/Route';
 import AccordionPage from './pages/AccordionPage';
 import DropdownPage from './pages/DropdownPage';
+import ButtonPage from './pages/ButtonPage';
 
 function App() {
 
   return (
-    <div>
-      <Link to="/accordion">Accordion</Link>
-      <Link to="/dropdown">Dropdown</Link>
-      <br />
-      <div>
+    <div className='container mx-auto grid grid-cols-6 gap-4 mt-4'>
+      <Sidebar />
+      
+      <div className='col-span-5'>
         <Route path="/accordion">
           <AccordionPage />
         </Route>
 
-        <Route path="/dropdown">
+        <Route path="/">
           <DropdownPage />
+        </Route>
+
+        <Route path="/button">
+          <ButtonPage />
         </Route>
       </div>
     </div>
